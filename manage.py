659c8c27d9f1643ede4bfeffe6387c65d9aba911
Mark Glasgow <markglasgow@gmail.com>
2019-03-01 19:42:17 +0000

    django + angular playing together

--- manage.py
+++ manage.py
@@ -2,8 +2,8 @@
 import os
 import sys
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_foodle.settings')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "angular_django_example.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
