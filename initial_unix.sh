8f06500272cde6bd9a0e8a0498660023dd8e1d44
Mark Glasgow <markglasgow@gmail.com>
2019-03-03 12:17:53 +0000

    bash initialisation scripts

--- initial_unix.sh
+++ initial_unix.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
+echo "Foodle : Repo initialisation Bash shell script"
 
-#echo "Configuring a local python 3.6 virtualenv"
-#pip3.6 install virtualenv
-#virtualenv -p python3.6 venv
-#. venv/bin/activate
-#echo "venv activated - installing project requirements.txt"
-cd Foodle_dev/
+echo "The follow file configures the following : "
+echo "Setting up venv + installing requirements.txt"
+echo "Install & Building npm"
+echo "serving using python manage.py runserver"
+echo "Estimated time : 1-5 minutes"
+
+echo "Configuring a local python 3.6 virtualenv"
+
+pip3.6 install virtualenv
+virtualenv -p python3.6 venv
+. venv/bin/activate
+echo "venv activated - installing project requirements.txt"
 pip3.6 install -r requirements.txt
 
 
 
 # Custom bash/sh script to enter into the correct directory. 
-echo ""Moving into the foodle/front-end directory and verifying there is no previous install of npm""
-
+echo ""Moving into the foodle/front-end directory""
+echo $PWD
 # foodle - An app within the project, containing the Django Rest Framework views and URL routing
-cd foodle/front-end
+cd foodle/front-end || exit
 
 # install globablly (within the environment)
 # npm cache verify
@@ -28,6 +35,7 @@ ng update @angular/cli @angular/core
 #  the local directory where npm install @angular/cli was run, 
 # which will use the locally installed angular-cli.
 
+echo "##### ng build... #####"
 
 # ng build command writes generated build artifacts to the output folder (/dist)
 ng build
@@ -36,12 +44,19 @@ echo "##### npm installed successfully. #####"
 
 # CD back into the home directory ./Foodle_dev
 cd ../..
+echo $PWD
 
 
 echo "##### manage.py runserver #####"
 
+
+
+
 # Run the python server - which is linked with angular via the REST framework. 
 python manage.py runserver
+
+echo "##### All configured successfully. #####"
+
 
 #webpack-bundle-tracker@0.4.2-beta
 #yarn add mjml@4.3.1
