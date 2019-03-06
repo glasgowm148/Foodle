4aa5b4bb2b27c5175e742cf49d8fe6f0876cfcf3
Nathan Kirkpatrick <nksocial99@gmail.com>
2019-03-06 00:16:48 +0000

    the amount of times I've had to clone this repo is getting out of hand

--- foodle/static/front-end/polyfills.js
+++ foodle/static/front-end/polyfills.js
@@ -5753,8 +5753,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/markglasgow/OneDrive - University of Glasgow/GitHub/Foodle_dev/foodle/front-end/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /Users/markglasgow/OneDrive - University of Glasgow/GitHub/Foodle_dev/foodle/front-end/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Users\Nathan\foodle_dev\foodle\front-end\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\Nathan\foodle_dev\foodle\front-end\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
