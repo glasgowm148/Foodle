097982dabdd42b57aa1c8fee2a8124e00bf99368
muhashh <overlordofdoom12345@gmail.com>
2019-03-19 16:30:53 +0000

    remove profile image folder for now

--- foodle/front-end/src/app/tab/tab.component.ts
+++ foodle/front-end/src/app/tab/tab.component.ts
@@ -1,21 +1,10 @@
 import { Component } from '@angular/core';
 
-<<<<<<< HEAD
-/**
- * @title Basic use of the tab group
- */
-@Component({
-  selector: 'tab.component',
-  templateUrl: 'tab.component.html',
-  styleUrls: ['tab.component.css'],
-
-=======
 @Component({
   selector: 'tab-component',
   templateUrl: './tab-component.html',
   styleUrls: ['./tab-component.css'],
 })
->>>>>>> 7fb98148a2eec016541002e19d8942693f11040b
 export class TabGroupBasicExample {}
 
 
