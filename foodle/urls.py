8c53db8a353c08cdd13a295336e1759c63a838e6
muhashh <overlordofdoom12345@gmail.com>
2019-03-19 00:01:29 +0000

    add JWT to URLs

--- foodle/urls.py
+++ foodle/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 from . import views
 
 router = routers.DefaultRouter(trailing_slash=False)
@@ -9,18 +9,19 @@ router.register(r'users', views.UserViewSet)
 
 
 urlpatterns = [
+
     path(r'api/', include(router.urls)),
     path(r'', views.index, name='index'),
     path(r'^about/$', views.about, name='about'),
     path(r'^deal_page/$', views.deal_page, name='deal_page'),
 
-
     # Login and Register
-    path(r'^register/$',
-        views.register,
-        name='register'),
+    path(r'^register/$', views.register, name='register'),
     path(r'^login/$', views.user_login, name='login'),
     path(r'^logout/$', views.user_logout, name='logout'),
 
+    # JWT
+    path(r'api-token-auth/', obtain_jwt_token),
+    path(r'api-token-refresh/', refresh_jwt_token),
 
 ]
