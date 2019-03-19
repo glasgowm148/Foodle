1043e7e83a227f98cc90037a899e7a95defa9ee2
Mark Glasgow <markglasgow@gmail.com>
2019-03-19 16:00:39 +0000

    more work on tabs

--- foodle/front-end/src/app/tab/tab.component.ts
+++ foodle/front-end/src/app/tab/tab.component.ts
@@ -1,9 +1,9 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'tab-component',
-  templateUrl: './tab-component.html',
-  styleUrls: ['./tab-component.css'],
+  templateUrl: 'tab-component.html',
+  styleUrls: ['tab-component.css'],
 })
 export class TabGroupBasicExample {}
 
@@ -11,3 +11,5 @@ export class TabGroupBasicExample {}
 /**  Copyright 2019 Google Inc. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
     can be found in the LICENSE file at http://angular.io/license */
+
+
