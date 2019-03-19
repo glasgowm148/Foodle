ad5f8d42a0d8c7eedbce7bfc2118c0447387810a
Mark Glasgow <markglasgow@gmail.com>
2019-03-19 15:49:51 +0000

    work on tab app

---
+++ foodle/front-end/src/app/tab/tab.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic use of the tab group
+ */
+@Component({
+  selector: 'tab.component',
+  templateUrl: 'tab.component.html',
+  styleUrls: ['tab.component.css'],
+})
+export class TabGroupBasicExample {}
+
+
+/**  Copyright 2019 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
