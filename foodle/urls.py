cced02ab444329249dc7dafa94574514400a059e
muhashh <overlordofdoom12345@gmail.com>
2019-03-19 00:49:47 +0000

    remove redundant JWT

--- foodle/urls.py
+++ foodle/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from rest_framework import routers
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 from . import views
 
 router = routers.DefaultRouter(trailing_slash=False)
@@ -21,9 +20,4 @@ urlpatterns = [
     path(r'^register/$', views.register, name='register'),
     path(r'^login/$', views.user_login, name='login'),
     path(r'^logout/$', views.user_logout, name='logout'),
-
-    # JWT
-    path(r'api-token-auth/', obtain_jwt_token),
-    path(r'api-token-refresh/', refresh_jwt_token),
-
 ]
