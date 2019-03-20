e396059e5ef682c192a5160cb0c4eddc9790c806
Mark Glasgow <markglasgow@gmail.com>
2019-03-20 13:59:21 +0000

    angular

--- foodle/front-end/src/app/app.component.ts
+++ foodle/front-end/src/app/app.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-tab',
   template: '<app-tab></app-tab>',
-  templateUrl: '.app.component.html'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {}
 
