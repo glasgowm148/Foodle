1d640bd8dfb03d4aed35e8cdf70e5067fa5869b6
Nathan Kirkpatrick <nksocial99@gmail.com>
2019-03-21 21:14:16 +0000

    model changes, progress on pop script

--- foodle/models.py
+++ foodle/models.py
@@ -2,12 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-class DealModel(models.Model):
+class Deal(models.Model):
     info = models.CharField(default='', max_length=200)
     picture = models.ImageField(upload_to='deals/', blank=True)
     likes = models.IntegerField(default=0)
-    category = models.CharField(default='', max_length=50)
+    
+    class Meta:
+        verbose_name_plural = 'deals'
 
+    def __str__(self):
+        return self.title
+    
     @property
     def add_like(self):
        self.likes += 1
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
