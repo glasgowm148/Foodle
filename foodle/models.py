fcd23524d19aed2c873fd633b8221bb557d503a8
muhashh <overlordofdoom12345@gmail.com>
2019-03-21 21:47:11 +0000

    fixed

--- foodle/models.py
+++ foodle/models.py
@@ -7,14 +7,10 @@ class DealModel(models.Model):
     picture = models.ImageField(upload_to='deals/', blank=True)
     likes = models.ManyToManyField(User, related_name='likes')
     category = models.CharField(default='', max_length=50)
-
-    def __str__(self):
-        return self.title
     
     @property
     def count_likes(self):
        return self.likes.count()
-
 
 
 class Category(models.Model):
@@ -25,3 +21,25 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class AboutPage(models.Model):
+    """
+    The "AboutPage" model for the foodle  app
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=timezone.now)
+    body = models.CharField(default='', max_length=200)
+
+    def __str__(self):
+        return self.body
+
+class BlogPost(models.Model):
+    """
+    The "BlogPost" model for the foodle  app
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=timezone.now)
+    body = models.CharField(default='', max_length=200)
+
+    def __str__(self):
+        return self.body
