4c704353b474ad4d39913b3ff522f9b1052a3b7f
Nathan Kirkpatrick <2283945k@student.gla.ac.uk>
2019-03-22 17:45:59 +0000

    commented the few tests we have

--- foodle/test.py
+++ foodle/test.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from .forms import SubmitForm
 
 class tests(TestCase):
+    # two required fields are fulfilled, form.is_valid should return true
     def test_info_and_category(self):
         form = SubmitForm({
             'info': "deal",
@@ -9,14 +10,14 @@ class tests(TestCase):
         })
         self.assertEquals(True, form.is_valid())
 
-    # check if 
+    # only one required field is fulfilled, form.is_valid should return false
     def test_if_no_info(self):
         form = SubmitForm({
             'category': "indian"
         })
         self.assertEquals(False, form.is_valid())
 
-    # if url is not a valid url, this should return False, so the test should pass
+    #  required fields are fulfilled but the url is not a valid url form.is_valid should return false
     def test_url(self):
         form = SubmitForm({
             'info': "deal",
