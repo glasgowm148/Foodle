a366224b56b3427da4ca121a2dd8e0476dbcace1
muhashh <overlordofdoom12345@gmail.com>
2019-03-23 03:38:17 +0000

    fix pop scipt and style deal_page

--- populate_foodle.py
+++ populate_foodle.py
@@ -9,36 +9,42 @@ from foodle.models import DealModel
 def populate():
     deals = [
         {
+            "name": "Vegan offer",
             "info": "Up to £5 off vegan items with a student card.",
             "likes": 12,
             "url": "https://www.fiveroffvegan.com",
             "category": "vegan"
         },
         {
+            "name": "Student card offer",
             "info": "A quarter off all purchases with a student card.",
             "likes": 38,
             "url": "https://www.quarteroffstudents.com",
             "category": "other"
         },
         {
+            "name": "Chinese offer",
             "info": "Save money on Chinese takeaway.",
             "likes": 22,
             "url": "https://www.chinesemoneyoff.com",
             "category": "asian"
         },
         {
+            "name": "Bar Soba offer",
             "info": "Bar Soba : 50% off for students.",
             "likes": 73,
             "url": "https://www.barsoba.com",
             "category": "asian"
         },
         {
+            "name": "Saucy Burger offer",
             "info": "Free cow with every extra saucy burger!",
             "likes": 322,
             "url": "https://www.saucyburger.com",
             "category": "indian"
         },
         {
+            "name": "Soy milk offer",
             "info": "Free soy milk (not oat) on purchases over £20.",
             "likes": 47,
             "url": "https://www.stopbuyingoats.com",
@@ -48,18 +54,22 @@ def populate():
 
 
     for deal in deals:
-        add_deal(deal["info"], deal["likes"], deal["url"], deal["category"])
+        add_deal(deal["name"], deal["info"], deal["likes"], deal["url"], deal["category"])
 
     for d in DealModel.objects.all():
         print("- {0}".format(str(d)))
 
-def add_deal(info, likes, url, category):
+def add_deal(name, info, likes, url, category):
     d = DealModel.objects.get_or_create(info=info)[0]
     
+    d.name=name
+    d.info=info
     d.likes=likes
     d.url=url
     d.category=category
+    
     d.save()
+
     return d
 
 if __name__ == '__main__':
