657376f03a3db02405f4e2eee1cfc78aa9f6309c
muhashh <overlordofdoom12345@gmail.com>
2019-03-23 16:37:44 +0000

    fix tests

--- foodle/test.py
+++ foodle/test.py
@@ -5,13 +5,14 @@ class tests(TestCase):
     # two required fields are fulfilled, form.is_valid should return true
     def test_info_and_category(self):
         form = SubmitForm({
-            'info': "deal",
+            'name': 'deal',
+            'info': "deal of something...",
             'category': "other"
         })
         self.assertEquals(True, form.is_valid())
 
     # only one required field is fulfilled, form.is_valid should return false
-    def test_if_no_info(self):
+    def test_if_no_info_and_name(self):
         form = SubmitForm({
             'category': "indian"
         })
@@ -20,8 +21,26 @@ class tests(TestCase):
     #  required fields are fulfilled but the url is not a valid url form.is_valid should return false
     def test_url(self):
         form = SubmitForm({
+            'name': 'deal',
             'info': "deal",
             'url': "deal",
             'category': "indian"
+        })
+        self.assertEquals(False, form.is_valid())
+    
+    def test_no_name(self) :
+        form = SubmitForm({
+            'info': "deal",
+            'url': "deal",
+            'category': "indian"
+        })
+        self.assertEquals(False, form.is_valid())
+    
+    def test_bad_picture_url(self) :
+        form = SubmitForm({
+            'info': "deal",
+            'url': "deal",
+            'category': "indian",
+            'picture': 'bad_link/bad.png'
         })
         self.assertEquals(False, form.is_valid())
